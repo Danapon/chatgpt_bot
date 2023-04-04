4c63ac2ce2bc09f3273378c8665e0a0aeaab8809
Danapon <117153333+Danapon@users.noreply.github.com>
2023-04-04 11:19:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        {"role": "system", "content": st.secrets.AppSettings.chatbot_setting}
         ]
 
 # チャットボットとやりとりする関数
